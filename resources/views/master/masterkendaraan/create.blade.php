04b91fb729a9a7f065787281ca2f4d605abd8995
bim-bima <pahlawanmalas@gmail.com>
2022-10-18 13:45:35 +0700

    update kendaraan

--- resources/views/master/masterkendaraan/create.blade.php
+++ resources/views/master/masterkendaraan/create.blade.php
@@ -6,11 +6,11 @@
     <form action="{{ route('master_kendaraan.store') }}" method="POST" enctype="multipart/form-data" class="px-0">
     @csrf
     <div class="card shadow-sm mb-4" data-aos="fade-right" data-aos-delay="50">
-      <div class="card-header py-3 px-3">
+      <div class="card-header py-3 px-sm-3 px-2">
         <h6 class="m-0 font-weight-bold text-primary" data-aos="fade-right" data-aos-delay="100">Status Kendaraan</h6>
       </div>
-      <div class="card-body px-1">
-        <div class="col-12 mb-2">
+      <div class="card-body px-0">
+        <div class="col-12 mb-2 px-sm-3 px-2">
           <label class="form-label">Status</label>
           <select name="mk_status" class="custom-select custom-select-md">
             <option value="">Status Kendaraan</option>
@@ -18,23 +18,23 @@
             <option value="sedang dipakai">Sedang Di Pakai</option>
           </select>
         </div>
-        <div class="col-12 mb-2">
+        <div class="col-12 mb-2 px-sm-3 px-2">
           <label for="bahan_bakar" class="form-label">Jumlah Bahan Bakar (Liter)</label>
           <input type="number" min="1" class="form-control @error('bahan_bakar') is-invalid @enderror" name="bahan_bakar" value="{{ old('bahan_bakar') }}" required>
           @error('bahan_bakar')
               <div class="invalid-feedback">{{ $message }}</div>
           @enderror
         </div>
-        <div class="col-12 mb-2">
+        <div class="col-12 mb-2 px-sm-3 px-2">
           <label for="kilometer" class="form-label">Kilometer (KM)</label>
           <input type="number" min="1" class="form-control @error('kilometer') is-invalid @enderror" name="kilometer" required value="{{ old('kilometer') }}">
           @error('kilometer')
           <div class="invalid-feedback">{{ $message }}</div>
           @enderror
         </div>
-        <div class="col-12 mb-2">
+        <div class="col-12 px-sm-3 px-2">
           <label for="kondisi_lain" class="form-label">Kondisi Lain</label>
-          <input type="text" class="form-control @error('kondisi_lain') is-invalid @enderror" name="kondisi_lain" value="{{ old('kondisi_lain') }} "required>
+          <textarea type="text" class="form-control @error('kondisi') is-invalid @enderror" name="kondisi_lain" required value="{{ old('kondisi_lain') }} " rows="3" >{{ old('kondisi_lain') }}</textarea>
           @error('kondisi_lain')
           <div class="invalid-feedback">{{ $message }}</div>
           @enderror
@@ -44,26 +44,26 @@
   </div>
   <div class="col-lg-6 pl-lg-1">
     <div class="card shadow-sm mb-4" data-aos="fade-left" data-aos-delay="50">
-      <div class="card-header py-3 px-3">
+      <div class="card-header py-3 px-sm-3 px-2">
         <h6 class="m-0 font-weight-bold text-primary" data-aos="fade-left" data-aos-delay="100">Data Kendaraan</h6>
       </div>
-      <div class="card-body px-sm-3">
+      <div class="card-body px-sm-3 px-2">
         <div class="row">
-          <div class="col-md-6 mb-2 pr-md-2">
+          <div class="col-md-6 mb-2 pr-md-1">
             <label for="nama_kendaraan" class="form-label">Nama Kendaraan</label>
             <input type="text" class="form-control @error('nama_kendaraan') is-invalid @enderror" name="nama_kendaraan" autofocus required value="{{ old('nama_kendaraan') }}">
             @error('nama_kendaraan')
             <div class="invalid-feedback">{{ $message }}</div>
             @enderror
           </div>
-          <div class="col-md-6 mb-2 pl-md-2">
+          <div class="col-md-6 mb-2 pl-md-1">
             <label for="no_polisi" class="form-label">No Polisi</label>
             <input type="text" class="form-control @error('no_polisi') is-invalid @enderror" name="no_polisi" required autofocus value="{{ old('no_polisi') }}">
             @error('no_polisi')
             <div class="invalid-feedback">{{ $message }}</div>
             @enderror
           </div>
-          <div class="col-md-6 mb-2 pr-md-2">
+          <div class="col-md-6 mb-2 pr-md-1">
             <label class="form-label">Jenis Kendaraan</label>
             <select name="mk_jenis" id="mk_jenis" class="custom-select custom-select-md">
               <option value="">Pilih Jenis Kendaraan</option>
@@ -71,21 +71,21 @@
               <option value="Roda 4">Roda empat (4)</option>
             </select>
           </div>
-          <div class="col-md-6 mb-2 pl-md-2">
+          <div class="col-md-6 mb-2 pl-md-1">
             <label for="merk" class="form-label">Merk Kendaraan</label>
             <input type="text" class="form-control @error('merk') is-invalid @enderror" name="merk" required autofocus value="{{ old('merk') }}">
             @error('merk')
             <div class="invalid-feedback">{{ $message }}</div>
             @enderror
           </div>
-          <div class="col-md-6 mb-2 pr-md-2">
+          <div class="col-md-6 mb-2 pr-md-1">
             <label for="warna" class="form-label">Warna Kendaraan</label>
             <input type="text" class="form-control @error('warna') is-invalid @enderror" name="warna" autofocus required value="{{ old('warna') }}" >
             @error('warna')
             <div class="invalid-feedback">{{ $message }}</div>
             @enderror
           </div>
-          <div class="col-md-6 mb-2 pl-md-2">
+          <div class="col-md-6 mb-2 pl-md-1">
             <label class="form-label">Perlengkapan</label>
             <select name="mk_perlengkapan" id="mk_perlengkapan" class="custom-select custom-select-md mb-4">
               <option value="">Kelengkapan Surat</option>
@@ -94,7 +94,7 @@
               <option value="BPKB">BPKB</option>
             </select>
           </div>
-          <div class="col-12 mt-2 mb-2">
+          <div class="col-12 mt-2 mb-5">
             <button class="btn btn-info mr-1">
               <i class="fa fa-angle-left"></i>
               <a href="{{ route('master_kendaraan.index') }}" class="text-white text-decoration-none">Kembali</a>
--- resources/views/master/masterkendaraan/edit.blade.php
+++ resources/views/master/masterkendaraan/edit.blade.php
@@ -3,66 +3,75 @@
 
 <div class="row">
   <div class="col-lg-6 pr-lg-1">
+    <form action="{{ route('master_kendaraan.update',$kendaraan->id) }}" method="POST" enctype="multipart/form-data" class="px-0">
+      @csrf
+      @method('put')
     <div class="card shadow-sm mb-4" data-aos="fade-right" data-aos-delay="50">
       <div class="card-header py-3 px-sm-3 px-2">
         <h6 class="m-0 font-weight-bold text-primary" data-aos="fade-right" data-aos-delay="100">Status Kendaraan</h6>
       </div>
-      <div class="card-body px-sm-3 px-2">  
-          <form action="{{ route('master_kendaraan.update',$kendaraan->id) }}" method="POST" enctype="multipart/form-data" class="row">
-          @csrf
-          @method('put')
-          <div class="col-12 mb-2">
-            <label for="mk_status" class="form-label">Status</label>
-            <select name="mk_status" class="form-control @error('mk_status') is-invalid @enderror" required>
-              <option value="{{ $kendaraan->mk_status }}">{{ $kendaraan->mk_status }}</option>
-              <option value="tersedia">Tersedia</option>
-              <option value="sedang dipakai">Sedang Di Pakai</option>
-            </select>
-              @error('mk_status')
-                <div class="invalid-feedback">{{ $message }}</div>
-              @enderror
-          </div>
-
-          <div class="col-12 mb-2">
-            <label for="Bahan_bakar" class="form-label">Bahan Bakar Tersedia</label>
-            <input type="number" class="form-control @error('bahan_bakar') is-invalid @enderror" name="bahan_bakar" required  value="{{ $kendaraan->mk_bahan_bakar }}">
-            @error('bahan_bakar')
-            <div class="invalid-feedback">{{ $message }}</div>
+      <div class="card-body px-0">  
+        <div class="col-12 mb-2 px-sm-3 px-2">
+          <label for="mk_status" class="form-label">Status</label>
+          <select name="mk_status" class="form-control @error('mk_status') is-invalid @enderror" required>
+            <option value="{{ $kendaraan->mk_status }}">{{ $kendaraan->mk_status }}</option>
+            <option value="tersedia">Tersedia</option>
+            <option value="sedang dipakai">Sedang Di Pakai</option>
+          </select>
+            @error('mk_status')
+              <div class="invalid-feedback">{{ $message }}</div>
             @enderror
-          </div>
+        </div>
+
+        <div class="col-12 mb-2 px-sm-3 px-2">
+          <label for="Bahan_bakar" class="form-label">Bahan Bakar Tersedia</label>
+          <input type="number" class="form-control @error('bahan_bakar') is-invalid @enderror" name="bahan_bakar" required  value="{{ $kendaraan->mk_bahan_bakar }}">
+          @error('bahan_bakar')
+          <div class="invalid-feedback">{{ $message }}</div>
+          @enderror
+        </div>
 
 
-          <div class="col-12 mb-1">
-            <label for="kilometer" class="form-label">Kilometer</label>
-            <input type="number" min="1" class="form-control @error('kilometer') is-invalid @enderror" name="kilometer" required  value="{{ $kendaraan->mk_kilometer }}">
-            @error('kilometer')
-            <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
-          </div>
+        <div class="col-12 mb-2 px-sm-3 px-2">
+          <label for="kilometer" class="form-label">Kilometer</label>
+          <input type="number" min="1" class="form-control @error('kilometer') is-invalid @enderror" name="kilometer" required  value="{{ $kendaraan->mk_kilometer }}">
+          @error('kilometer')
+          <div class="invalid-feedback">{{ $message }}</div>
+          @enderror
+        </div>
 
-<div class="col-12 mb-1">
-            <label for="kondisi_lain" class="form-label">Kondisi Lain</label>
-            <input type="text" class="form-control @error('kondisi') is-invalid @enderror" name="kondisi_lain" required  value="{{ $kendaraan->mk_kondisi_lain }}">
-            @error('kondisi_lain')
-            <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
-          </div>
-
-          <div class="col-md-6 mb-2">
+        <div class="col-12 px-sm-3 px-2">
+          <label for="kondisi_lain" class="form-label">Kondisi Lain</label>
+          <textarea type="text" class="form-control @error('kondisi') is-invalid @enderror" name="kondisi_lain" required value="{{ $kendaraan->mk_kondisi_lain }}" rows="3" >{{ $kendaraan->mk_kondisi_lain }}</textarea>
+          @error('kondisi_lain')
+          <div class="invalid-feedback">{{ $message }}</div>
+          @enderror
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-lg-6 pl-lg-1">
+    <div class="card shadow-sm mb-4" data-aos="fade-left" data-aos-delay="50">
+      <div class="card-header py-3 px-sm-3 px-2">
+        <h6 class="m-0 font-weight-bold text-primary" data-aos="fade-left" data-aos-delay="100">Data Kendaraan</h6>
+      </div>
+      <div class="card-body px-sm-3 px-2">
+        <div class="row">
+          <div class="col-md-6 mb-2 pr-md-1">
             <label for="nama_kendaraan" class="form-label">Nama Kendaraan</label>
             <input type="text" class="form-control @error('nama_kendaraan') is-invalid @enderror" name="nama_kendaraan" value="{{ $kendaraan->mk_nama_kendaraan }}" required>
             @error('nama_kendaraan')
             <div class="invalid-feedback">{{ $message }}</div>
             @enderror
           </div>
-          <div class="col-md-6 mb-2">
+          <div class="col-md-6 mb-2 pl-md-1">
             <label for="no_polisi" class="form-label">No Polisi</label>
             <input type="text" class="form-control @error('no_polisi') is-invalid @enderror" name="no_polisi" required value="{{ $kendaraan->mk_no_polisi }}">
             @error('no_polisi')
             <div class="invalid-feedback">{{ $message }}</div>
             @enderror
           </div>
-          <div class="col-md-6 mb-2">
+          <div class="col-md-6 mb-2 pr-md-1">
             <label for="mk_jenis" class="form-label">Jenis Kendaraan</label>
             <select name="mk_jenis" class="form-control @error('mk_jenis') is-invalid @enderror" required>
               <option value="{{ $kendaraan->mk_jenis }}">{{ $kendaraan->mk_jenis }}</option>
@@ -73,23 +82,23 @@
             <div class="invalid-feedback">{{ $message }}</div>
             @enderror
           </div>
-          <div class="col-md-6 mb-2">
+          <div class="col-md-6 mb-2 pl-md-1">
             <label for="merk" class="form-label">Merk Kendaraan</label>
             <input type="text" class="form-control @error('merk') is-invalid @enderror" name="merk" required  value="{{ $kendaraan->mk_merk }}">
             @error('merk')
             <div class="invalid-feedback">{{ $message }}</div>
             @enderror
           </div>
-          <div class="col-md-6 mb-2">
+          <div class="col-md-6 mb-2 pr-md-1">
             <label for="warna" class="form-label">Warna Kendaraan</label>
             <input type="text" class="form-control @error('warna') is-invalid @enderror" name="warna" required  value="{{ $kendaraan->mk_warna }}">
             @error('warna')
             <div class="invalid-feedback">{{ $message }}</div>
             @enderror
           </div>
-          <div class="col-md-6 mb-3">
+          <div class="col-md-6 mb-2 pl-md-1">
             <label for="mk_perlengkapan" class="form-label">Perlengkapan</label>
-              <select name="mk_perlengkapan" class="form-control @error('mk_perlengkapan') is-invalid @enderror" required>
+              <select name="mk_perlengkapan" class="mb-4 form-control @error('mk_perlengkapan') is-invalid @enderror" required>
                 <option value="{{ $kendaraan->mk_perlengkapan }}">{{ $kendaraan->mk_perlengkapan }}</option>
                 <option value="STNK-BPKB">STNK-BPKB</option>
                 <option value="STNK">STNK</option>
@@ -99,22 +108,22 @@
             <div class="invalid-feedback">{{ $message }}</div>
             @enderror
           </div>
-          <div class="col-12 mt-4">
-            <button class="btn btn-info mb-1 mr-1">
+          <div class="col-12 mt-2 mb-5">
+            <button class="btn btn-info mr-1">
               <i class="fa fa-angle-left"></i>
               <a href="{{ route('app_kendaraan.index') }}" class="text-white text-decoration-none">
                 Kembali
               </a>
             </button>
-            <button type="submit" class="btn btn-success mb-1">
+            <button type="submit" class="btn btn-success">
               <i class="fa fa-edit"></i>
               Edit
             </button>
-          </div>
-          
-        </form>
+          </div>          
+        </div>
       </div>
     </div>
+    </form>
   </div>
 </div>
 @endsection
